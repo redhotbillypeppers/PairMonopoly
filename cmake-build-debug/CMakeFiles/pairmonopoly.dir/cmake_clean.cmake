fefc7db8b430b24242c51427e147b97b592e9257
redhotbillypeppers <benisthebestintheworld@gmail.com>
2025-04-25 03:09:24 -0400

    what is happening

---
+++ cmake-build-debug/CMakeFiles/pairmonopoly.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pairmonopoly.dir/main.cpp.obj"
+  "CMakeFiles/pairmonopoly.dir/main.cpp.obj.d"
+  "libpairmonopoly.dll.a"
+  "pairmonopoly.exe"
+  "pairmonopoly.exe.manifest"
+  "pairmonopoly.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pairmonopoly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
